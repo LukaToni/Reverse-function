c152d7eb4d28914a89ab414533b336cbb83ffaac
Luka <luka.toni93@gmail.com>
2014-06-22 13:04:28 +0200

    Update RF.c

--- RF.c
+++ RF.c
@@ -7,24 +7,24 @@ int main()
 	char line[100];
 
 	while(gets(line) != EOF)
-    {
-        reverse(line);
-        printf("%s\n", line);
-    }
+	{
+		reverse(line);
+		printf("%s\n", line);
+	}
 
 	return 0;
 }
 
 int reverse(char string[])
 {
-  int len = strlen(string);
-  int i;
-  char tmp;
-  for(i = 0; i < len / 2; i = i + 1)
-  	{
-  	tmp = string[i];
-  	string[i] = string[len - i - 1];
-  	string[len - i - 1] = tmp;
-  	}
-  return 0;
+	int len = strlen(string);
+	int i;
+	char tmp;
+		for(i = 0; i < len / 2; i = i + 1)
+		{
+			tmp = string[i];
+			string[i] = string[len - i - 1];
+			string[len - i - 1] = tmp;
+		}
+	return 0;
 }
